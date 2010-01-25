573a7c774522d616c337b8c45710ff4d3ea758c5
Johannes Raggam <johannes@raggam.co.at>
2010-01-25 01:12:24 +0100

    removed unused view module

--- src/cal/venue/browser/venueview.py
+++
@@ -1,5 +0,0 @@
-from Products.Five.browser import BrowserView
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-
-class VenueView(BrowserView):
-    __call__ = ViewPageTemplateFile('venueview.pt')
